d4eb650ef226c4c4eec4c732a11c279d194f4726
Youssef Saeid <146128402+YoussefSaeidSaleh@users.noreply.github.com>
2026-04-16 13:29:33 +0200

    lesson-2

---
+++ features/cake/cakeSlice.js
@@ -0,0 +1,22 @@
+const createSlice = requir("@reduxjs/toolkit").createSlice;
+
+const initialState = {
+  numberOfCakes: 10,
+};
+
+const cakeSlice = createSlice({
+  name: "cake",
+  initialState,
+  reducer: {
+    ordered: (state) => {
+      state.numberOfCakes--;
+    },
+    reStocked: (state, action) => {
+      state.numberOfCakes += action.payload;
+    },
+  },
+});
+
+module.exports = cakeSlice.reducer;
+
+module.exports.cakeActions = cakeSlice.action;
