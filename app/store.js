52b6f85dc6e5b1b5c0d0f99a899231aea293303c
Youssef Saeid <146128402+YoussefSaeidSaleh@users.noreply.github.com>
2026-04-24 14:58:13 +0300

    lesson-6-7

--- app/store.js
+++ app/store.js
@@ -1,14 +1,14 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/iceCream/icecreamSlice");
-const { logger } = require("redux-logger");
+// const { logger } = require("redux-logger");
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
   },
-  middleware: (getDefaultMiddle) => getDefaultMiddle().concat(logger),
+  // middleware: (getDefaultMiddle) => getDefaultMiddle().concat(logger),
 });
 
 module.exports = store;
